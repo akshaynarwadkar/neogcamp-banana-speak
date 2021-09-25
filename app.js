4a6a547803d7c4f335e8d329645e517f3da3dc88
akshaynarwadkar <90238392+akshaynarwadkar@users.noreply.github.com>
2021-09-25 13:16:28 +0530

    Input translate output

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+ 
 var btntranslate = document.querySelector("#btn-Translate");
 var txtinput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
+
+// console.log(txtinput);
 // console.log(txtinput); -> when we take the cursor to that written part,this shows the exact block the written code is referring to
 // To read this value, we can store that console value as global variable temp and use temp.value to display the input
 
-btntranslate.addEventListener("click",function clickeventlistener(){
-    // console.log("clicked");
-    console.log("input", txtinput.value);
-})
+function clickHandler(){
+    outputDiv.innerText = "ksjdfnnj " + txtinput.value;
+};
+
+
+btntranslate.addEventListener("click", clickHandler);
